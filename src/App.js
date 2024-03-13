0c2540bfc7045069516f74b7aecfee7fb835e5cd
Craigm84 <craigmorris2907@gmail.com>
2024-03-13 15:33:13 +0000

    First Commit

--- src/App.js
+++ src/App.js
@@ -4,20 +4,8 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Welcome to My Page - not sure whats going on here yet but Hey!</h1>
+      <a href='https://github.com/Craigm84' target='blank'>My GitHub Profile</a>
     </div>
   );
 }
