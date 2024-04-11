3e304a48fb2d09670ee3803a7f0ce969cfa5570a
Craigm84 <craigmorris2907@gmail.com>
2024-04-11 15:13:00 +0100

    Players style tweak

--- src/Components/Player.jsx
+++ src/Components/Player.jsx
@@ -1,4 +1,3 @@
-import PropTypes from 'prop-types';
 import axios from 'axios';
 import { useEffect, useState } from "react";
 import Avatar from '../Avatar.png'
@@ -18,36 +17,39 @@ function usePlayers() {
 function Players() {
   const players = usePlayers();
   return (
-    <div className='row'>
-  {players.map(player => (
-    <div className='col-2' key={player.id}>
-      <div className='card'>
-        <div className='row'>
-          <div className='col-4'>
-            <img
-              src={Avatar}
-              className='card-img'
-              alt="boy"
-              style={{ maxWidth: "100%" }}
-            />
-          </div>
-          <div className='col-8'>
-            <div className='card-body'>
-              <h5 className='card-title'>Name: {player.name}</h5>
-              <div className='card-text'>
-                <ul className='list-group list-group-flush'>
-                  <li className='list-group-item'>Number: {player.number}</li>
-                  <li className='list-group-item'>Position: {player.position}</li>
-                  <li className='list-group-item'>Skill: {player.skill}</li>
-                </ul>
+    <div><h2 style={{ color:"blue"}}>Meet The Team</h2>
+
+      <div className='row'>
+        {players.map(player => (
+          <div className='col-3' key={player.id}>
+            <div className='card' style={{ marginBottom: "20px" }}>
+              <div className='row'>
+                <div className='col-4'>
+                  <img
+                    src={Avatar}
+                    className='card-img'
+                    alt="boy"
+                    style={{ maxWidth: "100%", marginTop: "10px", marginBottom: "10px" }}
+                  />
+                </div>
+                <div className='col-8'>
+                  <div className='card-body'>
+                    <h5 className='card-title'>Name: {player.name}</h5>
+                    <div className='card-text'>
+                      <ul className='list-group list-group-flush'>
+                        <li className='list-group-item'>Number: {player.number}</li>
+                        <li className='list-group-item'>Position: {player.position}</li>
+                        <li className='list-group-item'>Skill: {player.skill}</li>
+                      </ul>
+                    </div>
+                  </div>
+                </div>
               </div>
             </div>
           </div>
-        </div>
+        ))}
       </div>
     </div>
-  ))}
-</div>
 
   );
 }
