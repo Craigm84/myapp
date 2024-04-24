e81bf64f65869031d3dc0edb2b402dfd6fdb0650
Craigm84 <craigmorris2907@gmail.com>
2024-04-24 14:36:33 +0100

    tidied code

--- src/Components/Typewriter.jsx
+++ src/Components/Typewriter.jsx
@@ -1,14 +1,14 @@
 import TypeIt from "typeit-react";
 
 function Type() {
-    return ( <div className="App">
-    <h4><TypeIt style={{ color:"blue"}}>Heather St. John’s Football Club is a football club based in the village of Heather, Leicestershire, England.<br />
-                They are currently members of the Midland League Division One and play their home matches at St John’s Park.<br />
-                The club is known for its community involvement and includes an academy for 4-8 year olds, senior, junior, youth, and ladies teams.<br />
-                They are an FA Community Charter Standard Football Club, emphasizing opportunities for children of all ages, genders, and abilities, and providing a pathway from junior to senior football.
-</TypeIt></h4>
-</div>
-);
+    return (<div className="App">
+        <h4><TypeIt style={{ color: "blue" }}>Heather St. John’s Football Club is a football club based in the village of Heather, Leicestershire, England.<br />
+            They are currently members of the Midland League Division One and play their home matches at St John’s Park.<br />
+            The club is known for its community involvement and includes an academy for 4-8 year olds, senior, junior, youth, and ladies teams.<br />
+            They are an FA Community Charter Standard Football Club, emphasizing opportunities for children of all ages, genders, and abilities, and providing a pathway from junior to senior football.
+        </TypeIt></h4>
+    </div>
+    );
 }
 
 export default Type;
