9836e0c7bbce127e9945e812a6887a39ac67afa5
Craigm84 <craigmorris2907@gmail.com>
2024-05-08 22:01:25 +0100

    tidy up

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,7 +1,7 @@
 function Home() {
     return ( 
         <div>
-        <h1>Welcome to My Page - not sure whats going on here yet but Hey!</h1>
+        <h1>Welcome to My Page!</h1>
         <a href='https://github.com/Craigm84' target='blank'>My GitHub Profile</a>
         </div>
      );
